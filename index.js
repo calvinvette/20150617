3e664fe77f7eea3be16c17c4b0483cc36f78c027
calvinvette <calvin@dragontale.com>
2015-06-18 10:39:30 -0400

    Added FireBase comet call

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var customers = [ ];
 
+var weasleyCometDB = new Firebase('https://nz8cr24e0r7.firebaseio-demo.com/');
+
+
 // anonymous callback function
 // that gets invoked after the page loads
 window.onload = function() {
@@ -22,10 +25,24 @@ txtFirstName.value = "Ginny";
 txtLastName.value = "Weasley";
 txtPhoneNumber.value = "+44 0206 555-1212";
 
+function receiveCustomer(snapshot) {
+	if (snapshot.key().indexOf("custs") == 0) {
+		var custs = snapshot.val();
+		for (var prop in custs) {
+			var cust = JSON.parse(custs[prop]);
+			addCustomer(cust);
+		}
+	} // else not a customer record, probably pixels...
+}
+
+weasleyCometDB.on("child_added", receiveCustomer);
+//weasleyCometDB.on("child_removed", receiveCustomer); // TODO - add delete functionality
+weasleyCometDB.on("child_changed", receiveCustomer);
+
 var btnSubmit = document.getElementById("btnSubmit");
 btnSubmit.onclick = function(evt) {
 	console.log("You clicked the button!!!");
-	/*
+		/*
 	var cust = {
 		firstName : txtFirstName.value,
 		lastName : txtLastName.value,
@@ -38,16 +55,23 @@ btnSubmit.onclick = function(evt) {
 	cust.lastName = txtLastName.value;
 	cust.phoneNumber = txtPhoneNumber.value;
 
+	addCustomer(cust);
 
-	customers.push(cust);
+	var customerKeyName = "custs/" + cust.firstName + ":" + cust.lastName;
+	weasleyCometDB.child(customerKeyName).set(JSON.stringify(cust));
+
+
 	txtFirstName.value = "";
 	txtLastName.value = "";
 	txtPhoneNumber.value = ""; // reset pn field
 	//console.log(customers);
+};
 
+function addCustomer(cust) {
+	customers.push(cust);
 	clearTable();	
 	populateTable();
-};
+}
 
 function clearTable() {
 	// Clear out the table except for the header row
